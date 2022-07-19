372ffb97be49de0de6d70fb9ef0a8e4f3868d27b
EltoroWoolfy <destroyerskyrim@gmail.com>
2022-07-19 16:02:13 +0300

    ppl

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('fuck')
+print('what is your name?')
+name = input()
+print('Hello, ' + name + ', your age is?')
+age = input()
+print('where are your from?')
+place = input()
+print('New clint! ' + name + ',' + age + ' years old, from ' + place + '')
+
