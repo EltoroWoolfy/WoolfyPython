cbb1deb976db16d58eefd37783ccb3f54bf391cb
EltoroWoolfy <destroyerskyrim@gmail.com>
2022-07-19 16:16:37 +0300

    useradd

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 print('what is your name?')
 name = input()
-print('Hello, ' + name + ', your age is?')
+print('Hello, '+name+', your age is?')
 age = input()
 print('where are your from?')
 place = input()
-print('New clint! ' + name + ',' + age + ' years old, from ' + place + '')
-
+print('New clint! '+name+', '+age+' years old, from '+place+'')
